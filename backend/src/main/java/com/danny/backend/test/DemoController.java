a31ed428d0267984d6c5873510dab609772e90e0
0xdanny <dpaolone03@gmail.com>
2024-01-18 13:20:29 +1100

    Secured subscription endpoints fixed.

--- backend/src/main/java/com/danny/backend/test/DemoController.java
+++ backend/src/main/java/com/danny/backend/test/DemoController.java
@@ -1,16 +1,21 @@
 package com.danny.backend.test;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.danny.backend.auth.AuthService;
 import com.danny.backend.models.BaseResponse;
 
 @RestController
 @RequestMapping("/api/v1/demo-controller")
 public class DemoController {
+
+    @Autowired
+    private AuthService authService;
 
     @GetMapping
     public ResponseEntity<BaseResponse<String>> demoController() {
